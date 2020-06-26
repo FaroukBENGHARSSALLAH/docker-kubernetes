82782ed068d7710015325a972db0bf60148d7e34
farouk <farouk.bengarssallah@gmail.com>
2020-06-26 19:18:58 +0100

    docker

---
+++ docker/frontend/build/precache-manifest.3b2597d28111656df354bcd51ffc3996.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d96811a271220718c1c7",
+    "url": "/static/js/5.d96811a2.chunk.js"
+  },
+  {
+    "revision": "00b1d0b2a7b7503729a1",
+    "url": "/static/css/main.4db21d76.chunk.css"
+  },
+  {
+    "revision": "504503aeffed60ecdb19",
+    "url": "/static/js/xlsx.504503ae.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "782d13870155ec8dfe3e",
+    "url": "/static/js/2.782d1387.chunk.js"
+  },
+  {
+    "revision": "cebb5b10acd3f6266214",
+    "url": "/static/js/3.cebb5b10.chunk.js"
+  },
+  {
+    "revision": "c353d5f9dc19614b2309",
+    "url": "/static/js/4.c353d5f9.chunk.js"
+  },
+  {
+    "revision": "00b1d0b2a7b7503729a1",
+    "url": "/static/js/main.00b1d0b2.chunk.js"
+  },
+  {
+    "revision": "a96f7c3f639d24651c64",
+    "url": "/static/js/runtime~main.a96f7c3f.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "782d13870155ec8dfe3e",
+    "url": "/static/css/2.96be541a.chunk.css"
+  },
+  {
+    "revision": "e1093eebd972cee52727983b2b1b2130",
+    "url": "/index.html"
+  }
+];
